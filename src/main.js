69a9cab8c0acd8fb2b41e56c53be2ce299c60f31
Jerald Thomas <jeraldlt@uwm.edu>
2025-09-12 13:33:01 -0500

    trying to hardcode the command

--- src/main.js
+++ src/main.js
@@ -64,7 +64,7 @@ function run() {
     }
 
     startTime = new Date()
-    result = spawnSync('exit', ['9'], {timeout, env, stdio: 'inherit'})
+    result = spawnSync('bash', ['-c', 'exit 9'], {timeout, env, stdio: 'inherit'})
     endTime = new Date()
 
     score = result.status

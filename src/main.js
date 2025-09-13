a9e1f45ef2c2ce2ba51f284aca895c1e0e13d79f
Jerald Thomas <jeraldlt@uwm.edu>
2025-09-12 23:49:56 -0500

    cleaning up

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 const core = require('@actions/core')
-// const {DefaultArtifactClient} = require('@actions/artifact')
 const {spawnSync} = require('child_process')
 const fs = require('fs');
-// const github = require('@actions/github');
 
 const env = {
   PATH: process.env.PATH,
@@ -12,55 +10,13 @@ const env = {
   HOME: process.env.HOME,
 }
 
-// const artifact = new DefaultArtifactClient()
-
-function btoa(str) {
-  return Buffer.from(str).toString('base64')
-}
-
-function generateResult(status, testName, command, message, duration, score, maxScore) {
-  return {
-    version: 1,
-    status,
-    max_score: maxScore,
-    tests: [
-      {
-        name: testName,
-        status,
-        score,
-        message,
-        test_code: '',
-        filename: '',
-        line_no: 0,
-        duration,
-      },
-    ],
-  }
-}
-
-function getErrorMessageAndStatus(error, command) {
-  if (error.message.includes('ETIMEDOUT')) {
-    return { status: 'error', errorMessage: 'Command timed out' }
-  }
-  if (error.message.includes('command not found')) {
-    return { status: 'error', errorMessage: `Unable to locate executable file: ${command}` }
-  }
-  if (error.message.includes('Command failed')) {
-    return { status: 'fail', errorMessage: 'failed with exit code 1' }
-  }
-  return  { status: 'error', errorMessage: error.message }
-}
-
-async function run() {
+function run() {
   const testName = core.getInput('test-name', {required: true})
   const setupCommand = core.getInput('setup-command')
   const command = core.getInput('command', {required: true})
   const arguments = JSON.parse(core.getInput('arguments', {required: true}))
   const timeout = parseFloat(core.getInput('timeout') || 10) * 60000 // Convert to minutes
   const maxScore = parseInt(core.getInput('max-score') || 0)
-
-
-  // const reportName = 'report_' + github.context.sha.substring(0, 7)
 
   let output = ''
   let startTime
@@ -73,16 +29,10 @@ async function run() {
     }
 
     startTime = new Date()
-    // result = spawnSync(command, arguments, {timeout, env, stdio: 'inherit'})
     result = spawnSync(command, arguments, {timeout, env})
     endTime = new Date()
 
     score = result.status
-
-    core.notice("Output:", {
-      title: "Autograder output",
-      details: result.stdout.toString('utf8')
-    })
 
     if (result.stderr != null) {
       core.error(result.stderr.toString('utf8'), {
@@ -93,7 +43,6 @@ async function run() {
     core.setOutput('report', result.stdout.toString('utf8'))
 
     fs.writeFileSync(process.env.GITHUB_STEP_SUMMARY, result.stdout)
-
 
     totalPoints = score
     maxPoints = maxScore
@@ -118,48 +67,16 @@ async function run() {
     if (score < maxScore) {
       core.setFailed("Score less than " + maxScore)
     }
-
-    // status = score == maxScore ? "pass" : "fail"
-
-    // core.notice(status, {
-    //   title: "Autograding status"
-    // })
-
-    // result = generateResult(status, testName, command, result.stdout, endTime - startTime, score, maxScore)
-
-
-    
-    // const {id, size} = await artifact.uploadArtifact(
-    //   // name of the artifact
-    //   reportName,
-    //   // files to include (supports absolute and relative paths)
-    //   ['./' + reportName + '.txt'],
-    //   '.',
-    //   // {
-    //   //   // optional: how long to retain the artifact
-    //   //   // if unspecified, defaults to repository/org retention settings (the limit of this value)
-    //   //   retentionDays: 10
-    //   // }
-    // )
-
-    // core.notice(await id, {
-    //   title: "Artifact ID"
-    // })
-    
+   
   } catch (error) {
     endTime = new Date()
-    // const {status, errorMessage} = getErrorMessageAndStatus(error, command)
 
     core.warning(error.message, {
       title: "Autograding error"
     })
 
     core.setFailed("Encountered error")
-    
-    // result = generateResult(status, testName, command, errorMessage, endTime - startTime, 0, maxScore)
   }
-
-  // core.setOutput('result', btoa(JSON.stringify(result)))
 }
 
 run()
